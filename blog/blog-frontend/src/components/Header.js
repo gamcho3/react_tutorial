7a3663a4ca606d902e9c2e420d50f268b9be1a21
yongjin park <70016523+gamcho3@users.noreply.github.com>
2023-04-24 22:38:52 +0900

    add button

---
+++ blog/blog-frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+import React from "react";
+
+/**
+ * 회원가입/로그인 페이지의 레이아웃 담당
+ */
+
+const HeaderBlock = styled.div``;
+
+const Header = () => {
+  return <HeaderBlock></HeaderBlock>;
+};
+
+export default Header;
